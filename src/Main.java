c05dc2cde87a513889116138e566336ff3742c5f
Ahmed Levent Cetinkaya <ahmed.levent.buss@gmail.com>
2023-06-12 17:49:38 +0300

    I have successfully resolved the issue and code is ready to be merged in the sun branch

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         solar.sun="1";
         solar.moon="2";
         solar.planets="8";
+
+        FaturesSun Xron = new FaturesSun();
+        Xron.color="pink";
+        Xron.radius="23412341234123";
+        Xron.temperature="31424124k";
     }
 }
